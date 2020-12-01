92ba9b85fa1b761a55f0d694da68200f114869df
Juan David <juandaf56@gmail.com>
2020-12-01 16:42:35 -0500

    Subiendo proyecto a git db

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php 
 
-	$host = 'localhost';
-	$user = 'root';
-	$password = '';
-	$db = 'jlm';
+	$host = 'de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+	$user = 'de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+	$password = 'xtz3w4dkb9gdxa3b';
+	$db = 'dp099bssdbmvmsmx';
 
 
 	$conection = @mysqli_connect($host,$user,$password,$db);
