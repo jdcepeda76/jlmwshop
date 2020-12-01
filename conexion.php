d70fa11f9a503f22b112dca2b600f5988cf1077f
Juan David <juandaf56@gmail.com>
2020-12-01 16:47:13 -0500

    Subiendo proyecto a git db2

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php 
 
 	$host = 'de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-	$user = 'de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+	$user = 'p12ev49vux4k7zri';
 	$password = 'xtz3w4dkb9gdxa3b';
 	$db = 'dp099bssdbmvmsmx';
 
